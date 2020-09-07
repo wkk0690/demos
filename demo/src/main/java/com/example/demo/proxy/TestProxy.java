2f9a20cd78c1f5938057699b641c903e87e873cb
wkk <wkk0690@126.com>
2020-09-07 14:15:53 +0800

    代理测试

---
+++ demo/src/main/java/com/example/demo/proxy/TestProxy.java
@@ -0,0 +1,30 @@
+package com.example.demo.proxy;
+
+import com.example.demo.proxy.cglib.CglibProxyFactory;
+import com.example.demo.proxy.jdk.JDKProxyFactory;
+
+/**
+ * @author wkk
+ * @create 2020/09/07
+ * @desc
+ */
+public class TestProxy {
+
+    public static void main(String[] args) {
+
+        //jdk动态代理
+        UserService userService = new UserServiceImpl();
+        userService.say();
+        JDKProxyFactory factory = new JDKProxyFactory(userService);
+        UserService proxy = (UserService) factory.createProxy();
+        proxy.say();
+
+        System.out.println("---------------------------------------");
+
+        //cglib代理
+        CglibProxyFactory cglibProxyFactory = new CglibProxyFactory(userService);
+        UserService proxy1 = (UserService) cglibProxyFactory.createProxy();
+        proxy1.say();
+
+    }
+}
