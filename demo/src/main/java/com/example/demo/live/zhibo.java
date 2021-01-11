f38b638fbcfe3fbfcccef586945048ca98acb4a1
wkk <wkk0690@126.com>
2021-01-11 17:38:09 +0800

    直播

--- demo/src/main/java/com/example/demo/live/zhibo.java
+++ demo/src/main/java/com/example/demo/live/zhibo.java
@@ -4,9 +4,11 @@ package com.example.demo.live;
  * @author wkk
  * @create 2020/05/29
  * @desc
- * red5 https://github.com/Red5/red5-server
- * obs https://obsproject.com/download
- * 文章 https://blog.csdn.net/iamniconico/article/details/80581937
+ * https://github.com/Red5/red5-server  red5
+ * https://obsproject.com/download obs
+ * https://blog.csdn.net/iamniconico/article/details/80581937 文章
+ * 放到tomcat中, 并且 开启flash
+ * https://www.jianshu.com/p/590bc391a213 No compatible source was found for this video
  */
 public class zhibo {
 }
