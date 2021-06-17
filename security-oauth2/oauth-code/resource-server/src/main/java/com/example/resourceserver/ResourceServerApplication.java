00cf9af31ef821fbb0e0e7bae47dca2de950667b
wkk <wkk0690@126.com>
2021-06-17 11:35:54 +0800

    观察者模式

--- security-oauth2/oauth-code/resource-server/src/main/java/com/example/resourceserver/ResourceServerApplication.java
+++ security-oauth2/oauth-code/resource-server/src/main/java/com/example/resourceserver/ResourceServerApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo3;
+package com.example.resourceserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Demo3Application {
+public class ResourceServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Demo3Application.class, args);
+        SpringApplication.run(ResourceServerApplication.class, args);
     }
 
 }
