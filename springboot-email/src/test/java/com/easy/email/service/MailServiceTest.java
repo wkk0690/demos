4d68f1f4bce23a4d399e998ebbf0c51a06fa9ec6
wkk <wkk0690@126.com>
2021-06-17 16:19:52 +0800

    1

---
+++ springboot-email/src/test/java/com/easy/email/service/MailServiceTest.java
@@ -0,0 +1,18 @@
+package com.easy.email.service;
+
+import com.easy.email.EmailApplicationTests;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class MailServiceTest extends EmailApplicationTests {
+    @Autowired
+    private MailService mailService;
+
+    /**
+     * 测试简单邮件
+     */
+    @Test
+    public void sendSimpleMail() {
+        mailService.sendSimpleMail("wkk0690@126.com", "这是一封简单邮件", "这是一封普通的SpringBoot测试邮件");
+    }
+}
